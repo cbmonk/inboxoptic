c65e3aefa809542a825266395085956ad45d919a
cbmonk.base.eth <cbmonk.base.eth@gmail.com>
2025-01-09 16:31:39 +0800

    small UI adjustment

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -12,13 +12,15 @@ export default function LandingPage() {
 
   return (
     <div className="min-h-screen w-screen bg-gradient-to-br from-emerald-50 to-emerald-100 font-['Inter']">
-      <div className="w-full min-h-screen px-4 flex flex-col items-center justify-start pt-20">
+      <div className="w-full min-h-screen px-4 flex flex-col items-center justify-start pt-16">
         <Header />
+        <div className="mt-0 mb-12">
+          <Features />
+        </div>
         <GoogleScanButton scanInbox={scanInbox} isScanning={isScanning} />
         <OutlookScanButton scanInbox={scanInbox} isScanning={isScanning} />
         <YahooScanButton scanInbox={scanInbox} isScanning={isScanning} />
         <ProtonMailScanButton scanInbox={scanInbox} isScanning={isScanning} />
-        <Features />
         <SubscriptionList subscriptions={subscriptions} />
       </div>
     </div>
